f243489212f8b3358cd05f120ad4aabf2a3cae89
Obinna Nwabia <valinevite@gmail.com>
2020-12-02 02:18:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,6 +57,11 @@ end
 def runner
   # code runner here
   Welcome
-  
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  end
 end
     
