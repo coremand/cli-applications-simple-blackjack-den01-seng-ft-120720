bb55b791b608bda22b13c640da61ce67019e7ba0
Obinna Nwabia <valinevite@gmail.com>
2020-12-02 01:56:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,11 +15,12 @@ end
 
 def prompt_user
   # code #prompt_user here
+  
 end
 
 def get_user_input
   # code #get_user_input here
-  puts "Type 'h' to hit or 's' to stay"
+  
 end
 
 def end_game(card_total)
